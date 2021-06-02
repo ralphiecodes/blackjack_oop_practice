f1c64b7ede92ad6ecd175888d8ff20786aecbf91
ralphiecodes <ironware6@gmail.com>
2021-06-02 16:30:51 -0400

    fixed values

--- blackjack.py
+++ blackjack.py
@@ -5,7 +5,7 @@ suits =('Hearts', 'Diamonds', "Spades", "Clubs")
 #Ranks of Cards
 ranks = ('Two', 'Three', 'Four', 'Five', "Six", 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 #Dictionary of Ranks and the values associated with them
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace' : 14}
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace' : 11}
 playing = True
 
 class Card:
